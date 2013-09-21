0b23643ccb49b105e6674e9c0af2a1609ae63fde
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2013-09-21 22:52:39 +0000

    dropped RDR command

--- src/pl/ijestfajnie/circles/CircleCommands.java
+++ src/pl/ijestfajnie/circles/CircleCommands.java
@@ -7,7 +7,7 @@ public final class CircleCommands {
 	public final static String CMD_SET = "00100"; //assigns a field a value
 	public final static String CMD_ADD = "00101"; //adds a value to field's value
 	public final static String CMD_SUB = "00110"; //subtracts a value from field's value
-	public final static String CMD_RDR = "00111"; //pushes to the render
+	//public final static String CMD_RDR = "00111"; //pushes to the render
 	public final static String CMD_SST = "01000"; //sets screen memory field
 	public final static String CMD_SAD = "01001"; //add to screen memory
 	public final static String CMD_SSU = "01010"; //subtract from screen memory
